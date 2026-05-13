3852ddd9e1d56f54bebd69b1d1afc68b847b48b3
José Carlos Salazar Acencio <22470072@campeche.tecnm.mx>
2026-05-12 20:05:40 -0600

    Primera version de la aplicacion prototipo SIARA, Frontend

---
+++ ProyectoSIARA/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuProyectoMVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard() => View();
+        public IActionResult Asignaturas() => View();
+        public IActionResult Graficas() => View();
+
+        // LAS DOS NUEVAS RUTAS
+        public IActionResult DocumentosIniciales() => View();
+        public IActionResult DetalleDocente() => View();
+    }
+}
---
+++ ProyectoSIARA/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class AuthController : Controller
+    {
+        // Pantalla 1
+        public IActionResult Login() => View();
+
+        // Pantalla 2
+        public IActionResult Recuperar() => View();
+
+        // Pantalla 3
+        public IActionResult Restablecer() => View();
+    }
+}
